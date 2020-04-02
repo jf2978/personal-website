e7ca0ec396f5171acef3d41b230d4885f22814d4
Jeffrey Fabian <jf2978@columbia.edu>
2020-04-02 12:18:09 -0400

    [Gatsby Foundation] Set up basic gatsby (static) site
    part 2: using scss + sample button
    
    step 3: hello world markdown blog post
    
    part 4: create a list of dummy blog posts
    
    part 5: add thumbnails to blog list
    
    part 6: add images to posts themselves
    
    part 7: adding tags to blogs
    
    part 8: project page frrom JSON data
    
    part 9: add pagination
    
    part 9: add some google analytics tracking

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -5,3 +5,6 @@
  */
 
 // You can delete this file if you're not using it
+
+require('prismjs/themes/prism.css'); // default theme; preview them again at https://github.com/PrismJS/prism/tree/1d5047df37aacc900f8270b1c6215028f6988eb1/themes
+require('prismjs/plugins/line-numbers/prism-line-numbers.css');
---
+++ src/pages/blog/hw2.md
@@ -0,0 +1,11 @@
+---
+path: /blog/hello-world
+title: Hello World
+date: 2019-12-29
+thumbnail: "../../images/placeholder-img.png"
+
+---
+
+This is my first blog post. Hello World 2!
+
+![Placeholder](../../images/placeholder-img.png)
