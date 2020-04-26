92cddbe767e751104e1a7d2773f2b1c390dc27d1
Jeffrey Fabian <jf2978@columbia.edu>
2020-04-26 12:54:39 -0400

    finish narrating comments for createPages

--- gatsby-node.js
+++ gatsby-node.js
@@ -18,6 +18,7 @@ exports.createPages = ({ actions, graphql }) => {
   const tagTemplate = path.resolve(`src/templates/tags-template.js`);
   const blogListTemplate = path.resolve(`./src/templates/blog-list-template.js`);
 
+  // query for markdown stuff
   return graphql(`
     {
       allMarkdownRemark(
@@ -37,12 +38,15 @@ exports.createPages = ({ actions, graphql }) => {
       }
     }
   `).then(result => {
+
+    // if that query returned some error, reject that promise
     if (result.errors) {
       return Promise.reject(result.errors);
     }
 
     const posts = result.data.allMarkdownRemark.edges;
 
+    // iterate through posts and create a page for each
     posts.forEach(({ node }) => {
       createPage({
         path: node.fields.slug,
@@ -50,18 +54,19 @@ exports.createPages = ({ actions, graphql }) => {
         context: { slug: node.fields.slug }, // additional data can be passed via context
       });
     });
-    // create Tags pages
-    // pulled directly from https://www.gatsbyjs.org/docs/adding-tags-and-categories-to-blog-posts/#add-tags-to-your-markdown-files
+
+    // Iterate through each post, if a tag value exists add it the tags array
     let tags = [];
-    // Iterate through each post, putting all found tags into `tags`
     _.each(posts, edge => {
       if (_.get(edge, 'node.frontmatter.tags')) {
         tags = tags.concat(edge.node.frontmatter.tags);
       }
     });
+
     // Eliminate duplicate tags
     tags = _.uniq(tags);
-    // Make tag pages
+
+    // Make tag pages (e.g. see all content related to "cats" tag)
     tags.forEach(tag => {
       createPage({
         path: `/tags/${_.kebabCase(tag)}/`,
@@ -71,10 +76,13 @@ exports.createPages = ({ actions, graphql }) => {
         },
       });
     });
+
     // Create blog post list pages
     const postsPerPage = 2;
     const numPages = Math.ceil(posts.length / postsPerPage);
 
+    // Construct of where length = numPages, iterate through the array by index
+    // and create pages for each based on the page it should be on
     Array.from({ length: numPages }).forEach((_, i) => {
       createPage({
         path: i === 0 ? `blog/` : `blog/${i + 1}`,
