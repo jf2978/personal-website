1758ab5061cedc392f5cf5c302e4fba6a4f0111f
Jeffrey Fabian <jf2978@columbia.edu>
2020-04-30 18:24:59 -0400

    make siteMetadata.siteUrl a real url to avoid config load failure

--- gatsby-config.js
+++ gatsby-config.js
@@ -5,7 +5,7 @@
 module.exports = {
   siteMetadata: {
     title: `My Journey`,
-    siteUrl: `jfabian.me`,
+    siteUrl: `https://jfabian.me`,
     description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
     author: `@jf2978`,
   },
