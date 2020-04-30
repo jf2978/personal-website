6365ff07106154be81eced88cdbe08e7fd6cc4df
Jeffrey Fabian <jf2978@columbia.edu>
2020-04-30 19:10:37 -0400

    npm prune + fix semantic styling issues (+ less@3.11.1 updated 1 package and audited 37593 packages in 26.549s
    150 packages are looking for funding
      run `npm fund` for details
    
    found 0 vulnerabilities)

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -1,13 +1,38 @@
 import React from "react"
 import PropTypes from "prop-types"
+import { Segment, Menu } from "semantic-ui-react"
 
 import "./layout.css"
 import '../styles/styles.scss';
+import 'semantic-ui-less/semantic.less';
+
+import styles from "./styles/navbar.module.scss"
+
+console.log(styles)
 
 const NavBar = ({ children }) => {
+
   return (
     <>
-
+      <Segment inverted>
+        <Menu inverted pointing secondary>
+          <Menu.Item
+            name='home'
+            //active={activeItem === 'home'}
+            //onClick={this.handleItemClick}
+          />
+          <Menu.Item
+            name='messages'
+            //active={activeItem === 'messages'}
+            //onClick={this.handleItemClick}
+          />
+          <Menu.Item
+            name='friends'
+            //active={activeItem === 'friends'}
+            //onClick={this.handleItemClick}
+          />
+        </Menu>
+      </Segment>
     </>
   )
 }
