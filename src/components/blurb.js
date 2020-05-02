43d57b86b7bdefa64b01be22bda5826fb43382c0
Jeffrey Fabian <jf2978@columbia.edu>
2020-05-02 11:24:13 -0400

    backtrack: no need for placeholder on text

--- src/components/blurb.js
+++ src/components/blurb.js
@@ -1,44 +1,25 @@
 import React from "react"
-import { Container, Placeholder, PlaceholderParagraph } from "semantic-ui-react"
 import "./layout.css"
 import "../styles/styles.scss";
 import "semantic-ui-less/semantic.less";
 
-class TextBlurb extends React.Component {
 
-  state = { loading: false }
+const HomepageText = () => {
 
-  placeholderBlurbText = () => {
-    return (
-      <Placeholder>
-        <PlaceholderParagraph>
-          <Placeholder.Line length='medium' />
-          <Placeholder.Line length='medium' />
-          <Placeholder.Line length='medium' />
-          <Placeholder.Line length='medium' />
-        </PlaceholderParagraph>
-      </Placeholder>
-    )
-  }
-
-  blurbText = () => {
-    return (
+  return (
+    // fun fact: this <> ... </> syntax is a short syntax for a React "fragment"
+    // Fragments are nice because they give us the benefit of chunking JSX (like containers)
+    // without actually rendering anything (bonus that it cleans up the DOM)
+    <>
       <p>
-        Some other text!
+      Professional software engineer, passionate thinker and aspiring creator — at least that's who I think I am.
+      Instead of trying to organize my nuanced, fluid and vivid life experiences into simple, discrete and digestible bullet points for the sake of filling a job description, I just want to start documenting my life.
+      I created this site to share my reflections and incomplete thoughts, not to publish a curated snapshot of who I want you to think I am.
+      And maybe along te way, I'll start to get a better sense of who I actually am.
       </p>
-    )
-  }
-
-  render() {
-    const { loading } = this.state
-
-    return (
-      <Container>
-        { loading ? this.placeholderBlurbText() : this.blurbText() }
-      </Container>
-    )
-  }
+    </>
+  )
 }
 
-export default TextBlurb
+export default HomepageText
 
