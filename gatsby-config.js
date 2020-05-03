3b818eaa0e56d3363ee882f8b58156d9ce846dc0
Jeffrey Fabian <jf2978@columbia.edu>
2020-05-03 11:06:02 -0400

    install CSS-in-JS lib (emotion) + refactor CTA

--- gatsby-config.js
+++ gatsby-config.js
@@ -86,6 +86,12 @@ module.exports = {
           }
         ],
       },
-    }
+    },
+    {
+      resolve: `gatsby-plugin-emotion`,
+      options: {
+        // Accepts all options defined by `babel-plugin-emotion` plugin.
+      },
+    },
   ]
 }
