70610e7dfc0b3beabbdd52ee43e5099da7e46534
Jeffrey Fabian <jf2978@columbia.edu>
2020-05-02 20:41:14 -0400

    fix font and align homepage header

--- src/components/header.js
+++ src/components/header.js
@@ -1,42 +1,16 @@
 import { Link } from "gatsby"
 import PropTypes from "prop-types"
 import React from "react"
+import { Header, Container } from "semantic-ui-react"
 
-const Header = ({ siteTitle }) => (
-  <header
-    style={{
-      background: `rebeccapurple`,
-      marginBottom: `1.45rem`,
-    }}
-  >
-    <div
-      style={{
-        margin: `0 auto`,
-        maxWidth: 960,
-        padding: `1.45rem 1.0875rem`,
-      }}
-    >
-      <h1 style={{ margin: 0 }}>
-        <Link
-          to="/"
-          style={{
-            color: `white`,
-            textDecoration: `none`,
-          }}
-        >
-          {siteTitle}
-        </Link>
-      </h1>
-    </div>
-  </header>
-)
+import headerStyles from "../components/styles/header.module.scss"
 
-Header.propTypes = {
-  siteTitle: PropTypes.string,
+const HomepageHeader = ( { title }) => {
+  return (
+    <Header textAlign="centered">
+      <p className={headerStyles.header}> {title} </p>
+    </Header>
+  )
 }
 
-Header.defaultProps = {
-  siteTitle: ``,
-}
-
-export default Header
+export default HomepageHeader
